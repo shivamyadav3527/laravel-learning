f0562b8a1590d91b2a4821bd5cb31a3a01c18200
shivam.yadav <shivam.yadav@happiestminds.com>
2025-03-18 10:35:21 +0530

    stubs customization done

---
+++ app/Http/Controllers/checkController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class checkController extends Controller
+{
+    // Shivam Yadav customized this feature by using stubs
+
+}
